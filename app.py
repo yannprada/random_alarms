321c09b5a049015ed03b589367e370e307a77d7a
yannprada <pradayrol.yannick@gmail.com>
2025-07-04 13:48:33 +0200

    time entry validation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ sys.path.append('../yaml_tkinter')
 import yamltk
 import tkinter as tk
 import time
+import re
 
 
 class Root(tk.Tk):
@@ -64,6 +65,44 @@ class Alarm(tk.Frame):
 
 class TimeEntry(tk.Entry):
     yaml_file = 'time_entry.yaml'
+    patterns = [
+        r'^[0-2]$',
+        r'^([01]\d|2[0-3])$',
+        r'^([01]\d|2[0-3]):$',
+        r'^([01]\d|2[0-3]):[0-5]$',
+        r'^([01]\d|2[0-3]):[0-5]\d$',
+        r'^([01]\d|2[0-3]):[0-5]\d:$',
+        r'^([01]\d|2[0-3]):[0-5]\d:[0-5]$',
+        r'^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$',
+    ]
+    
+    def init(self, var_name):
+        self.var = tk.StringVar()
+        self.var.set('00:00:00')
+        self.configure(textvariable=self.var)
+        
+        self.no_user_input = True
+        self.bind('<FocusIn>', self.on_focus_in)
+        self.bind('<KeyRelease>', self.on_key_release)
+    
+    def on_focus_in(self, event):
+        if self.no_user_input:
+            self.no_user_input = False
+            self.var.set('')
+    
+    def on_key_release(self, event):
+        if not self.validate(self.get()):
+            i = self.index("insert")
+            self.delete(i-1)
+    
+    def validate(self, time_string):
+        l = len(time_string)
+        if l > 8:
+            return False
+        elif l == 0:
+            return True
+        pattern = self.patterns[l-1]
+        return re.match(pattern, time_string)
 
 
 def wrap_int(x, maxi):
