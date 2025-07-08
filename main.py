a6dbc0d7fcb948ba46c943df893f2b5ccf3b6f7d
yannprada <pradayrol.yannick@gmail.com>
2025-07-08 12:30:37 +0200

    separated app.py into main.py and root.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('../yaml_tkinter')
+sys.path.append('../tk_double_scale')
+import yamltk
+from tk_double_scale import DoubleScale as _DoubleScale
+
+import tkinter as tk
+
+from root import Root, Alarm, AlarmSound, AlarmTime
+from time_entry import TimeEntry
+from alarm_container import AlarmContainer
+from alarm_appearance import AlarmAppearance
+
+
+class DoubleScale(tk.Frame):
+    def inner_config(self, kwargs):
+        self._scale = _DoubleScale(self, **kwargs)
+        self._scale.pack()
+    
+    def get_values(self):
+        return NotImplementedError()
+
+
+if __name__ == '__main__':
+    branches = [Alarm, AlarmContainer, TimeEntry, AlarmSound, AlarmTime, 
+        AlarmAppearance, DoubleScale]
+    builder = yamltk.Builder(Root, branches)
+    
+    button = builder.tk_widgets['button_add']
+    alarm_container = builder.tk_widgets['alarm_container']
+    button.configure(command=alarm_container.add)
+    alarm_container.update_display()
+    
+    builder.root._update()
+    builder.root.mainloop()
