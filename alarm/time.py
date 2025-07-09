7828fc14b962b5dea6e66098c9caae227ffc9155
yannprada <pradayrol.yannick@gmail.com>
2025-07-09 11:29:25 +0200

    added another time option

--- alarm/time.py
+++ alarm/time.py
@@ -3,14 +3,15 @@ import tkinter as tk
 
 class AlarmTime(tk.LabelFrame):
     yaml_file = 'alarm/time.yaml'
-    every_ids = ['alarm_every_label', 'alarm_every_entry']
+    repeat_ids = ['alarm_every_label', 'alarm_every_entry',
+                  'alarm_to_label', 'alarm_to_entry']
     
     def init(self):
-        self.toggle_grid(self.every_ids, False)
+        self.toggle_grid(self.repeat_ids, False)
     
     def on_repeat_button(self):
         repeat = self.tk_variables['alarm_repeat']
-        self.toggle_grid(self.every_ids, repeat.get())
+        self.toggle_grid(self.repeat_ids, repeat.get())
     
     def toggle_grid(self, ids, visible):
         for id in ids:
