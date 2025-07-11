2ba4ae2f68dffde461df950e5897cbbe4435cfbf
yannprada <pradayrol.yannick@gmail.com>
2025-07-11 16:39:26 +0200

    added start save stop buttons

--- alarm/time.py
+++ alarm/time.py
@@ -3,19 +3,17 @@ import tkinter as tk
 
 class AlarmTime(tk.LabelFrame):
     yaml_file = 'alarm/time.yaml'
-    repeat_ids = ['alarm_every_label', 'alarm_every_entry',
-                  'alarm_to_label', 'alarm_to_entry']
     
     def init(self):
-        self.toggle_grid(self.repeat_ids, False)
+        self.frame_repeat = self.builder.tk_widgets['frame_repeat']
+        self.toggle_grid(False)
     
     def on_repeat_button(self):
         repeat = self.tk_variables['alarm_repeat']
-        self.toggle_grid(self.repeat_ids, repeat.get())
+        self.toggle_grid(repeat.get())
     
-    def toggle_grid(self, ids, visible):
-        for id in ids:
-            if visible:
-                self.builder.tk_widgets[id].grid()
-            else:
-                self.builder.tk_widgets[id].grid_remove()
+    def toggle_grid(self, visible):
+        if visible:
+            self.frame_repeat.grid()
+        else:
+            self.frame_repeat.grid_remove()
