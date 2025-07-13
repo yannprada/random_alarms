7e6541c2a65d6c9cc403647da084e8cb18d5d463
yannprada <pradayrol.yannick@gmail.com>
2025-07-13 07:03:33 +0200

    minor

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ from tk_time_picker import TimePicker as _TimePicker
 
 import tkinter as tk
 
-from root import Root
-from color_button import ColorButton
-from time_entry import TimeEntry
-from alarm.container import AlarmContainer
-from alarm.alarm import Alarm
-from alarm.appearance import AlarmAppearance
-from alarm.sound import AlarmSound
-from alarm.time import AlarmTime
-from alarm.run import AlarmRun
-
 
 class DoubleScale(tk.Frame):
     def inner_config(self, kwargs):
@@ -43,15 +33,27 @@ class TimePicker(tk.Frame):
         self._picker.set_value(value)
 
 
+from root import Root
+from color_button import ColorButton
+from alarm.container import AlarmContainer
+from alarm.alarm import Alarm
+from alarm.appearance import AlarmAppearance
+from alarm.sound import AlarmSound
+from alarm.time import AlarmTime
+from alarm.run import AlarmRun
+
+
+BRANCHES = [AlarmContainer, Alarm, AlarmAppearance, AlarmSound, AlarmTime, AlarmRun,
+            ColorButton, DoubleScale, TimePicker]
+
+
 if __name__ == '__main__':
-    branches = [AlarmContainer, Alarm, AlarmAppearance, AlarmSound, AlarmTime, AlarmRun,
-                ColorButton, DoubleScale, TimeEntry, TimePicker]
-    builder = yamltk.Builder(Root, branches)
+    builder = yamltk.Builder(Root, BRANCHES)
     
     button_add = builder.root.children['!frame'].children['button_add']
     alarm_container = builder.root.children['alarm_container']
     button_add.configure(command=alarm_container.add)
     alarm_container._update()
-        
+    
     builder.root._update()
     builder.root.mainloop()
