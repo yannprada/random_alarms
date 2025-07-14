e1bb97899652c572904664da2c5d90bd7dc72b37
yannprada <pradayrol.yannick@gmail.com>
2025-07-14 10:24:08 +0200

    used new hepler in yaml: app_command_event

--- alarm/run.py
+++ alarm/run.py
@@ -3,15 +3,3 @@ import tkinter as tk
 
 class AlarmRun(tk.LabelFrame):
     yaml_file = 'alarm/run.yaml'
-    
-    def on_save_run_button(self):
-        self.event_generate('<<SAVE_RUN>>')
-    
-    def on_save_button(self):
-        self.event_generate('<<SAVE>>')
-    
-    def on_stop_button(self):
-        self.event_generate('<<STOP>>')
-    
-    def on_remove_button(self):
-        self.event_generate('<<REMOVE>>')
