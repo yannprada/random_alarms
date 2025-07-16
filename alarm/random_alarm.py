40c2c69f7b48c506c41758cd9b2973848d60d5c2
yannprada <pradayrol.yannick@gmail.com>
2025-07-16 23:18:41 +0200

    added logic for sound

--- alarm/random_alarm.py
+++ alarm/random_alarm.py
@@ -1,3 +1,4 @@
+import pygame.midi
 import random
 import tkinter as tk
 
@@ -13,7 +14,38 @@ import tkinter as tk
 # 'notes_amount': [6, 6]}
 
 # Class in charge spawning the actual message, and play the notes
-class RandomAlarm:
+class RandomAlarm(tk.Toplevel):
     def __init__(self, appearance_data, sound_data):
+        super().__init__()
         self.appearance_data = appearance_data
         self.sound_data = sound_data
+        
+        pygame.midi.init()
+        self.midi_player = pygame.midi.Output(device_id=0)
+        
+        self.remaining_notes = random.randint(*self.sound_data['notes_amount'])
+        self.play()
+    
+    def play(self):
+        if self.remaining_notes <= 0:
+            self.destroy()
+        
+        self.remaining_notes -= 1
+        
+        # http://www.ccarh.org/courses/253/handout/gminstruments/
+        instrument = random.randint(*self.sound_data['instruments'])
+        self.midi_player.set_instrument(instrument)
+        
+        note = random.randint(*self.sound_data['notes'])
+        self.midi_player.note_on(note, self.sound_data['volume'])
+        
+        delay = random.uniform(*self.sound_data['notes_length'])
+        self.after(int(delay * 1000), lambda: self.note_off(note))
+    
+    def note_off(self, note):
+        self.midi_player.note_off(note, self.sound_data['volume'])
+        self.play()
+    
+    def __del__(self):
+        del self.midi_player
+        pygame.midi.quit()
